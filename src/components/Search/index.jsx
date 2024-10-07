f8613c0b9c7b1b367f11cfbab87b32073ebfd250
Yang Zhou <yangzhouinter@gmail.com>
2024-10-07 22:56:53 +1100

    update state before and after axios for list component to display proper info

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -7,15 +7,26 @@ export default class Search extends Component {
     const {
       keyWordElement: { value: keyWord },
     } = this;
-
+    /////////////////////////////////// update the state just before sending request ///////////////////////////////////
+    this.props.updateState({
+      firstTimeMounted: false, // change the status of the first mount to false because the component has been mounted
+      isLoading: true, // change the loading status to true because the request is being sent
+    });
     // send axios request
     axios.get(`http://localhost:3000/api1/search/users?q=${keyWord}`).then(
       (response) => {
-        this.props.saveUsers(response.data.items);
+        ////////////////////////////////// update the state before receiving response //////////////////////////////////
+        this.props.updateState({
+          users: response.data.items, // store the array of users
+          isLoading: false, // change the loading status to false because the response has been received
+        });
       },
       (error) => {
-        // update the state
-        console.log(error);
+        //////////////////////////////////// update the state after receiving error ////////////////////////////////////
+        this.props.updateState({
+          isLoading: false, // change the loading status to false because the error has been received
+          err: error.message, // store the error message
+        });
       }
     );
   };
