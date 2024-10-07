fa464a5903c28f292c4aab3793840e90eaf8deb4
Yang Zhou <yangzhouinter@gmail.com>
2024-10-07 23:15:29 +1100

    conditionally display info in the list component

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -3,24 +3,32 @@ import "./index.css";
 
 export default class List extends Component {
   render() {
-    const { users, firstTimeMounted, isLoading, err } = this.props.state;
+    const { users, firstTimeMounted, isLoading, err } = this.props;
 
     return (
       <div className="row">
-        {users.map((userObj) => {
-          return (
-            <div className="card" key={userObj.id}>
-              <a rel="noreferrer" href={userObj.html_url} target="_blank">
-                <img
-                  src={userObj.avatar_url}
-                  alt="profile"
-                  style={{ width: "100px" }}
-                />
-              </a>
-              <p className="card-text">{userObj.login}</p>
-            </div>
-          );
-        })}
+        {firstTimeMounted ? (
+          <h2>Enter the name you want to search</h2>
+        ) : isLoading ? (
+          <h2>Loading...</h2>
+        ) : err ? (
+          <h2 style={{ color: "red" }}>{err}</h2>
+        ) : (
+          users.map((userObj) => {
+            return (
+              <div className="card" key={userObj.id}>
+                <a rel="noreferrer" href={userObj.html_url} target="_blank">
+                  <img
+                    src={userObj.avatar_url}
+                    alt="profile"
+                    style={{ width: "100px" }}
+                  />
+                </a>
+                <p className="card-text">{userObj.login}</p>
+              </div>
+            );
+          })
+        )}
       </div>
     );
   }
